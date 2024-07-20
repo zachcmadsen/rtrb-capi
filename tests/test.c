2405f99b3411a682bd140d379bd14ea7de88bae9
Zach Madsen <zach@zachmadsen.com>
2024-07-19 20:20:24 -0700

    Add a CHECK macro for tests

--- tests/test.c
+++ tests/test.c
@@ -4,23 +4,24 @@
 
 #include <rtrb.h>
 
+#define CHECK(cond)                                                                                                    \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if (!(cond))                                                                                                   \
+        {                                                                                                              \
+            passed = false;                                                                                            \
+            goto cleanup;                                                                                              \
+        }                                                                                                              \
+    } while (false)
+
 bool test_write_and_read()
 {
     bool passed = true;
 
     rtrb *rb = rtrb_new(6);
 
-    if (rtrb_write_available(rb) != 6)
-    {
-        passed = false;
-        goto cleanup;
-    }
-
-    if (rtrb_read_available(rb) != 0)
-    {
-        passed = false;
-        goto cleanup;
-    }
+    CHECK(rtrb_write_available(rb) == 6);
+    CHECK(rtrb_read_available(rb) == 0);
 
     /*
      * Write and read twice to cover the case where the first and second slices
@@ -31,42 +32,22 @@ bool test_write_and_read()
     {
         uint8_t src[] = {1, 2, 3, 4};
         size_t written = rtrb_write(rb, src, sizeof(src));
-        if (written != sizeof(src))
-        {
-            passed = false;
-            goto cleanup;
-        }
+        CHECK(written == sizeof(src));
 
         uint8_t dst[] = {0, 0, 0, 0};
         size_t read = rtrb_read(rb, dst, sizeof(dst));
-        if (read != sizeof(dst))
-        {
-            passed = false;
-            goto cleanup;
-        }
+        CHECK(read == sizeof(dst));
 
         size_t j;
         for (j = 0; j < sizeof(dst); ++j)
         {
-            if (src[j] != dst[j])
-            {
-                passed = false;
-                goto cleanup;
-            }
+
+            CHECK(src[j] == dst[j]);
         }
     }
 
-    if (rtrb_write_available(rb) != 6)
-    {
-        passed = false;
-        goto cleanup;
-    }
-
-    if (rtrb_read_available(rb) != 0)
-    {
-        passed = false;
-        goto cleanup;
-    }
+    CHECK(rtrb_write_available(rb) == 6);
+    CHECK(rtrb_read_available(rb) == 0);
 
 cleanup:
     rtrb_free(rb);
